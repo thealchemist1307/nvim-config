971d49bf6e912a1c2241eec7c2ec2913169b7900
thealchemist1307 <46978534+thealchemist1307@users.noreply.github.com>
2025-12-17 19:06:02 +0530

    new changes

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -7,15 +7,29 @@ return {
       under_cursor = true,
       large_file_cutoff = 2000,
       min_count_to_highlight = 2,
-      providers = { "lsp", "treesitter", "regex" },
+      filetype_overrides = {
+        javascriptreact = { providers = { "lsp", "treesitter", "regex" } },
+        typescriptreact = { providers = { "lsp", "treesitter", "regex" } },
+      },
     },
     config = function(_, opts)
-      require("illuminate").configure(opts)
+      local illuminate = require("illuminate")
+      illuminate.configure(opts)
 
-      -- Make highlights visible in most themes
-      vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "LspReferenceText" })
-      vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "LspReferenceRead" })
-      vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "LspReferenceWrite" })
+      -- make it unmissable (themes sometimes make defaults invisible)
+      vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = true })
+      vim.api.nvim_set_hl(0, "IlluminatedWordRead", { underline = true })
+      vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { underline = true })
+      vim.api.nvim_set_hl(0, "IlluminatedCurWord", { underline = true, bold = true })
+    end,
+  },
+
+  -- ensure parsers exist
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "javascript", "typescript", "tsx", "html", "css", "json" })
     end,
   },
 }
