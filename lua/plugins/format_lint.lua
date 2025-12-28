f80ce34ce2b28c6cf19e0c17f6077457aba8c04c
thealchemist1307 <nishitrchouhan@gmail.com>
2025-12-29 01:14:40 +0530

    new changes

--- lua/plugins/format_lint.lua
+++ lua/plugins/format_lint.lua
@@ -3,7 +3,6 @@ return {
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
-      opts.format_on_save = { timeout_ms = 2000, lsp_fallback = true }
       opts.formatters_by_ft = vim.tbl_extend("force", opts.formatters_by_ft or {}, {
         -- Web
         javascript = { "prettier" },
